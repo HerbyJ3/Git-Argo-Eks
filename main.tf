fed93ecd7b88a9cb56d047718a8edeaf9a1eaefc
Herby <jeanty.herby@gmail.com>
2025-04-27 18:57:21 -0500

    removed region from main and add var to providers

--- main.tf
+++ main.tf
@@ -1,18 +1,16 @@
 # Root main.tf
 module "vpc" {
   source = "./modules/vpc"
-
-  region              = var.region
+  # Source path to the VPC module
   vpc_cidr_block      = var.vpc_cidr_block
   public_subnet_cidrs = var.public_subnet_cidrs
 }
 
 module "eks" {
   source = "./modules/eks"
-
+  # Source path to the EKS module
   cluster_name       = var.cluster_name
   vpc_id             = module.vpc.vpc_id
   subnet_ids         = module.vpc.public_subnet_ids
-  region             = var.region
   
 }
